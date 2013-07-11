e63508dcc6ca30e016069e8d846a134b3c8ba6b5
dasu <desusp@gmail.com>
2013-07-11 16:12:14 -0400

    Update sc2.sh

--- sc2.sh
+++ sc2.sh
@@ -3,6 +3,6 @@ user="$1"
 i="0"
 while [ $i -lt $2 ]
 do
-   curl "https://api.soundcloud.com/users/$user/favorites.json?consumer_key=apigee&limit=200&offset=$i"|sed 's/\,/\n/g'|grep \"permalink_url\":\"http:\/\/soundcloud.com\/.*/.*\" >> ~/test.txt
+   curl "https://api.soundcloud.com/users/$user/favorites.json?consumer_key=apigee&limit=200&offset=$i"|sed 's/\,/\n/g'|grep \"permalink_url\":\"http:\/\/soundcloud.com\/.*/.*\"|sed 's/\"permalink_url\"://g' >> ~/test.txt
 i=$[$i+200]
 done
