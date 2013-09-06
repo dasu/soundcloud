3ffc795392ec417cf9d4897f513192fedefb8153
dasu <desusp@gmail.com>
2013-09-06 14:21:38 -0400

    Update soundcrawl.py

--- soundcrawl.py
+++ soundcrawl.py
@@ -16,9 +16,9 @@ for users in followers:
 	o = 0
 	favs = client.get ('/users/%s/favorites'%(user.permalink), limit = 200, offset = o)
 	while favs:
-			favs = client.get ('/users/%s/favorites'%(user.permalink), limit = 200, offset = o)
-			o = o+200
-			for x in favs:
-				print x.permalink_url
-				text.write(x.permalink_url + '\n')
-				#client.get ('/users/user/favorites', created_at = { 'from':'2012-08-29 12:13:23'}) (used to file songs by date)
+		favs = client.get ('/users/%s/favorites'%(user.permalink), limit = 200, offset = o)
+		o = o+200
+		for x in favs:
+			print x.permalink_url
+			text.write(x.permalink_url + '\n')
+			#client.get ('/users/user/favorites', created_at = { 'from':'2012-08-29 12:13:23'}) (used to file songs by date)
