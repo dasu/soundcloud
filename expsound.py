c4c952cf83e582b85f3ab4b73fc4c255dd7933f5
dasu <desusp@gmail.com>
2013-09-27 12:12:56 -0400

    Create expsound.py

---
+++ expsound.py
@@ -0,0 +1,42 @@
+# expsound.py v0.13 proof of concept
+# Using the experimental soundcloud API, finally gather only links that were favorited after x date
+# to do: speed it up, write to file, idk
+
+
+import soundcloud
+import json
+import urllib
+from datetime import *
+
+client = soundcloud.Client(
+        client_id='',
+        client_secret='',
+        username='',
+        password='')
+
+def sound(uri):
+        bytes = urllib.urlopen(uri)
+        test = bytes.read()
+        m = json.loads(test)
+        return m
+
+followers = client.get('/me/followings')
+
+for users in followers:
+        o = 0
+        uri = "https://api.soundcloud.com/e1/users/%s/likes.json?client_id=YOUR_CLIENT_ID&limit=200&offset=%s"%(users.id,o)
+        m = sound(uri)
+        while m:
+                uri = "https://api.soundcloud.com/e1/users/%s/likes.json?client_id=YOUR_CLIENT_ID&limit=200&offset=%s"%(users.id,o)
+                m = sound(uri)
+                o = o + 200
+                for songs in m:
+                        created = songs['created_at']
+                        cre = datetime.strptime(created[:19], "%Y/%m/%d %H:%M:%S")
+                        bef = datetime.today() - timedelta(days=26)
+                        if bef < cre:
+                                try:
+                                        print songs['track']['permalink_url'], cre
+                                except TypeError:
+                                        print "Playlist maybe"
+                                        #print cre, bef < cre
