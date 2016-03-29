f46ea357d9e7ae688b2b967b83cf52cd5c467435
dasu <desusp@gmail.com>
2016-03-29 10:26:10 -0400

    Update soundcloud.sh

--- soundcloud.sh
+++ soundcloud.sh
@@ -3,7 +3,8 @@
 # useful for just grabbing whatever has changed.
 #v.23
 
-date=$(date +%F)
+date1=$(date +%F)
+date2=$(date -r diff.txt +%F)
 
 python ./soundcrawl.py
 
@@ -13,6 +14,12 @@ sort -u ./new.txt -o ./new.txt
 
 comm -3 ./new.txt ./old.txt| sed 's/^\t//' > ./diff.txt
 
-mv ./old.txt ./old/old-$date.txt
+mv ./old.txt ./old/old-$date1.txt
 
 mv ./new.txt ./old.txt
+
+python ./expsound.py $date2
+
+cat ./test5.txt ./diff.txt > ./combined.txt
+
+sort -u ./combined.txt -o ./combined.txt
